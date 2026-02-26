6a95cc2954761517a89691b85ba80637b05eea35
Maksym <maksymRozhko7@gmail.com>
2026-02-26 19:39:02 +0200

    new 5 pages for services

--- src/js/components/initSliders.js
+++ src/js/components/initSliders.js
@@ -25,3 +25,36 @@ new Swiper('.swiper.advantage-slider', {
     type: 'fraction',
   },
 });
+
+new Swiper('.swiper.setup-slider', {
+  slidesPerView: 1,
+  speed: 1000,
+  spaceBetween: 30,
+
+  pagination: {
+    el: '.setup .setup-slider__controls.swiper-pagination',
+    type: 'fraction',
+  },
+});
+
+new Swiper('.swiper.process-slider', {
+  slidesPerView: 1,
+  speed: 1000,
+  spaceBetween: 30,
+
+  pagination: {
+    el: '.process .process-slider__controls.swiper-pagination',
+    type: 'fraction',
+  },
+});
+
+new Swiper('.swiper.approach-slider', {
+  slidesPerView: 1,
+  speed: 1000,
+  spaceBetween: 30,
+
+  pagination: {
+    el: '.approach .approach-slider__controls.swiper-pagination',
+    type: 'fraction',
+  },
+});
